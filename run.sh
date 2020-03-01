8cb6d75afbc2d3a3b53d73cd0c89f58bae3c4026
Artur Tarassow <atecon@posteo.de>
2020-03-01 12:26:18 +0100

    rename shell-script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+DIR=$(dirname $(realpath "$0")) 	# locate folder where this sh-script is located in
+
+cd $DIR
+echo "Switched to ${DIR}"
+
+gretlcli -b -e -q ./script/run.inp
+
+if [ $? -eq 0 ]
+then
+  echo "Finished analysis"
+  rm string_table.txt
+  exit 0
+else
+  echo "Failure: Some error occured."
+  rm string_table.txt
+  exit 1
+fi
+
+
